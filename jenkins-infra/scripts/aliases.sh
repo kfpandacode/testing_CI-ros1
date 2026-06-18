9c9c1bc251f6400fd54fb080e70a6a2c9e26f743
tesarect <karthikn.balasubramanian@gmail.com>
2026-06-18 18:10:45 +0530

    clean up

--- jenkins-infra/scripts/aliases.sh
+++ jenkins-infra/scripts/aliases.sh
@@ -1,3 +1,14 @@
 
 # SMEE_URL (webhook)
 export SMEE_URL="https://smee.io/WOi7gUgFDGQcVAQ"
+
+alias srcrc="source ~/.bashrc"
+
+# git
+cleanpull() {
+    git reset --hard HEAD
+    git clean -fd
+    git pull
+}
+
+# newgrp docker
