e859f6215f0a9f87a2e206defd1d3581d7203041
tesarect <karthikn.balasubramanian@gmail.com>
2026-06-19 21:05:40 +0530

    aliases with log outputs for pass and fail case form jenkins

--- jenkins-infra/scripts/aliases.sh
+++ jenkins-infra/scripts/aliases.sh
@@ -3,6 +3,7 @@
 export SMEE_URL="https://smee.io/WOi7gUgFDGQcVAQ"
 
 alias srcrc="source ~/.bashrc"
+alias tdir="cd ~/simulation_ws/src/ros1_ci"
 
 # git
 cleanpull() {
@@ -11,4 +12,25 @@ cleanpull() {
     git pull
 }
 
-# newgrp docker
+dkradd() {
+    newgrp docker
+}
+
+killcont() {
+    kill $(cat ~/webpage_ws/jenkins/jenkins.pid)
+}
+
+simbld() {
+    cd  ~/simulation_ws/src/ros1_ci
+    docker build -t tortoisebot-noetic-gazebo:latest .
+}
+
+startjenkins() {
+    cd ~/simulation_ws/src/ros1_ci
+    bash jenkins-infra/scripts/jenkins_bootstrap.sh
+}
+
+installplugins() {
+    cd ~/simulation_ws/src/ros1_ci
+    bash jenkins-infra/scripts/install_plugins.sh
+}
