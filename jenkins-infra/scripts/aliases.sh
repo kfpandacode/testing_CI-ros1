c2c4082365f2b56c0e18b7edfa19cec8b677a650
tesarect <karthikn.balasubramanian@gmail.com>
2026-06-23 13:15:54 +0530

    unified ros1 and ros2 alisases

--- jenkins-infra/scripts/aliases.sh
+++ jenkins-infra/scripts/aliases.sh
@@ -1,36 +1,36 @@
 
-# SMEE_URL (webhook)
-export SMEE_URL="https://smee.io/WOi7gUgFDGQcVAQ"
+# # SMEE_URL (webhook)
+# export SMEE_URL="https://smee.io/WOi7gUgFDGQcVAQ"
 
-alias srcrc="source ~/.bashrc"
-alias tdir="cd ~/simulation_ws/src/ros1_ci"
+# alias srcrc="source ~/.bashrc"
+# alias tdir="cd ~/simulation_ws/src/ros1_ci"
 
-# git
-cleanpull() {
-    git reset --hard HEAD
-    git clean -fd
-    git pull
-}
+# # git
+# cleanpull() {
+#     git reset --hard HEAD
+#     git clean -fd
+#     git pull
+# }
 
-dkradd() {
-    newgrp docker
-}
+# dkradd() {
+#     newgrp docker
+# }
 
-killcont() {
-    kill $(cat ~/webpage_ws/jenkins/jenkins.pid)
-}
+# killcont() {
+#     kill $(cat ~/webpage_ws/jenkins/jenkins.pid)
+# }
 
-simbld() {
-    cd  ~/simulation_ws/src/ros1_ci
-    docker build -t tortoisebot-noetic-gazebo:latest .
-}
+# tbld() {
+#     cd  ~/simulation_ws/src/ros1_ci
+#     docker build -t tortoisebot-noetic-gazebo:latest .
+# }
 
-startjenkins() {
-    cd ~/simulation_ws/src/ros1_ci
-    bash jenkins-infra/scripts/jenkins_bootstrap.sh
-}
+# startjenkins() {
+#     cd ~/simulation_ws/src/ros1_ci
+#     bash jenkins-infra/scripts/jenkins_bootstrap.sh
+# }
 
-installplugins() {
-    cd ~/simulation_ws/src/ros1_ci
-    bash jenkins-infra/scripts/install_plugins.sh
-}
+# installplugins() {
+#     cd ~/simulation_ws/src/ros1_ci
+#     bash jenkins-infra/scripts/install_plugins.sh
+# }
